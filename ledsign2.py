304de44c36eb996b0d625c9ff772382a394559c0
Michael Farrell <micolous+hg@gmail.com>
2010-09-14 02:27:09 +0930

    added licensing data.  was always licensed under lgpl3, this merely re-enforces it.

--- ledsign2.py
+++ ledsign2.py
@@ -4,6 +4,20 @@ Driver for the Bestlink Optoelectronics M500N-7X80RG2
 Copyright 2010 Michael Farrell <http://micolous.id.au/>
 
 Requires pyserial library in order to interface.
+
+
+This library is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 import serial, string
 from datetime import datetime, time

3ee209f2d77fc311fe4261030bfd3276f1ceb181
Michael Farrell <micolous+git@gmail.com>
2011-07-13 17:36:31 +0930

    added some more model number alternatives for M500N-7X80RG2

--- ledsign2.py
+++ ledsign2.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 """
-Driver for the Bestlink Optoelectronics M500N-7X80RG2
+Driver for the Bestlink Optoelectronics M500N-7X50R1 / M500N-7X80RG2
 Copyright 2010 Michael Farrell <http://micolous.id.au/>
 
-This sign is also sometimes known as XC-0198 (as sold by Jaycar and Procon 
-Technology in AU).
+This sign is also sometimes known as XC-0191, XC-0193 or XC-0198 (as sold by
+Jaycar and Procon Technology in AU).
 
 Requires pyserial library in order to interface.
 
